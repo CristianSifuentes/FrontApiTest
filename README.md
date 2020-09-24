91ab337c5562304ec052d413322b62a2bebe03b8
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2020-09-24 17:33:27 -0500

    20200924: We have started the config app angular client

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ This is a repository where we show javascripts skills creating a client and api
 
 # Commands used in angular client 
 * npm install -g @angular/cli
+* ng g c header
+* npm install --save @angular/material @angular/cdk
+
---
+++ client/src/app/service/jsonplaceholder.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Photo } from '../model/Photo';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JsonplaceholderService {
+
+  constructor(private http: HttpClient) { }
+
+  getPhotos() {
+    return this.http.get<Photo[]>('https://jsonplaceholder.typicode.com/photos?_limit=5');
+  }
+}
